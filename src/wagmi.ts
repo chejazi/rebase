8a35623233d4efcf857d240a13c72960c33b2ed5
Cameron Hejazi <chejazi@umich.edu>
2025-01-14 15:37:54 -0600

    Connector fix

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -3,17 +3,18 @@ import { http, createConfig } from "wagmi";
 import { base } from "wagmi/chains";
 import { farcasterFrame } from "@farcaster/frame-wagmi-connector";
 
-export const config = createConfig(
-  getDefaultConfig({
-    chains: [base],
-    walletConnectProjectId: 'aa5ce756d374d1956cc6489edcb9446c',
-    appName: "Rebase",
-    transports: {
-      [base.id]: http(),
-    },
-    connectors: [farcasterFrame()],
-  }),
-);
+const defaultConfig = getDefaultConfig({
+  chains: [base],
+  walletConnectProjectId: 'aa5ce756d374d1956cc6489edcb9446c',
+  appName: "Rebase",
+  transports: {
+    [base.id]: http(),
+  },
+});
+// @ts-ignore: TS2339
+defaultConfig.connectors?.push(farcasterFrame());
+
+export const config = createConfig(defaultConfig);
 
 declare module "wagmi" {
   interface Register {
